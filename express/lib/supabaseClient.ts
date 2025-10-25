5af21ad9da93e1014763d3ce15e56cd38ca0fae5
vihashah <viha.shah@sjsu.edu>
2025-10-25 11:45:34 -0700

    database access works

--- express/lib/supabaseClient.ts
+++ express/lib/supabaseClient.ts
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 import dotenv from "dotenv";
-dotenv.config();
+dotenv.config({ path: "../../.env" });
 
 export const supabase = createClient(
   process.env.SUPABASE_URL!,
