add22599b98a6c52634b2bcd6ffc4b3f78091173
ninaw04 <ninawangcn@gmail.com>
2025-10-25 17:49:00 -0700

    :sparkles: topics and uploading content mockup

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import { Sidebar } from '../components/Sidebar';
-// import { TopicsPage } from './components/TopicsPage';
-// import { UploadContent } from './components/UploadContent';
+import { TopicsPage } from '../components/TopicsPage';
+import { UploadContent } from '../components/UploadContent';
 // import { FlashcardsPage } from './components/FlashcardsPage';
 // import { QuizPage } from './components/QuizPage';
 // import { TutorStudentPage } from './components/TutorStudentPage';
@@ -37,10 +37,10 @@ export default function App() {
     switch (currentPage) {
       case 'dashboard':
         return <DashboardPage />;
-      // case 'topics':
-      //   return <TopicsPage />;
-      // case 'upload':
-      //   return <UploadContent />;
+      case 'topics':
+        return <TopicsPage />;
+      case 'upload':
+        return <UploadContent />;
       // case 'flashcards':
       //   return <FlashcardsPage />;
       // case 'quiz':
