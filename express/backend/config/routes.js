4a2d524ac5b98c67b7b9071367de48c44ebe82c4
Viha Shah <47764272+Viha123@users.noreply.github.com>
2025-10-26 07:36:22 -0700

    :explosion: analytics page yippee (#26)
    Co-authored-by: Krishna Tagdiwala <krishna.tagdiwala@gmail.com> and Nina Wang <nina.wangcn@gmail.com>

--- express/backend/config/routes.js
+++ express/backend/config/routes.js
@@ -1,10 +1,12 @@
 var indexRouter = require("../routes/index");
 var tagsRouter = require("../routes/tags");
 var flashcardsRouter = require("../routes/flashcards");
+var analyticsRouter = require("../routes/analytics");
 
 module.exports = (app) => {
   app.use("/", indexRouter);
   app.use("/tags", tagsRouter);
   app.use("/flashcards", flashcardsRouter);
+  app.use("/analytics", analyticsRouter);
   // !-- Do not remove this line --! //
 };
